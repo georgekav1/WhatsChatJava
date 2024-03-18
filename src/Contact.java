32dd1c3bd8ecee001b81f5477d7e8d7a29976cfe
georgekav1 <149398975+georgekav1@users.noreply.github.com>
2024-03-18 22:03:12 +0000

    Commenting code bro

--- src/Contact.java
+++ src/Contact.java
@@ -1,20 +1,43 @@
-
+/**
+ * The Contact class represents a contact with a name and phone number.
+ */
 public class Contact {
-	private String name;
-	private String phoneNumber;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    private String name;
+    private String phoneNumber;
 
-	
+    /**
+     * Retrieves the name of the contact.
+     *
+     * @return The name of the contact.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the contact.
+     *
+     * @param name The new name of the contact.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Retrieves the phone number of the contact.
+     *
+     * @return The phone number of the contact.
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * Sets the phone number of the contact.
+     *
+     * @param phoneNumber The new phone number of the contact.
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import javax.swing.*;
 import GUIs.*;
-
 import java.awt.FlowLayout;
 
+/**
+ * The Main class contains the main method to start the application.
+ */
 public class Main {
-	public static void main(String[] args) {
-		Landing frame = new Landing();
-		
-		frame.setVisible(true);
-		
-		
-	}
+    /**
+     * The main method is the entry point of the application.
+     * It creates a new instance of the Landing class and makes it visible.
+     *
+     */
+    public static void main(String[] args) {
+        Landing frame = new Landing();
+
+        frame.setVisible(true);
+    }
 }
